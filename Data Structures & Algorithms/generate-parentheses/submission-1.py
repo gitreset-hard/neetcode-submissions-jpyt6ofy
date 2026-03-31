7e155f5ade896be6cb55a5ccaa7e40ab45000172
letsgetit <71613715+gitreset-hard@users.noreply.github.com>
2026-03-31 15:53:52 -0600

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        res = []
+
+        openP, closeP = n, n
+
+        def dfs(openP, closeP, curPath):
+            # leaf
+            if closeP == openP == 0 :
+                res.append(curPath)
+                return
+        # can only close once there has been one opened
+            if closeP - openP > 0:
+                dfs(openP, closeP-1, curPath + ")")
+            #can't close -> open
+            if openP  > 0:
+                dfs(openP-1, closeP, curPath + "(" )
+
+            
+            
+        
+        dfs(n,n,"")
+        return res

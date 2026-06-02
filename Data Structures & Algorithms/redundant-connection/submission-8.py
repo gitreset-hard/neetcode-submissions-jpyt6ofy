82afc52bbdb17b3bf0c5801490d7c68ed92d9961
letsgetit <71613715+gitreset-hard@users.noreply.github.com>
2026-06-01 22:07:42 -0600

    Add: redundant-connection - submission-8

---
+++ Data Structures & Algorithms/redundant-connection/submission-8.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        """
+        idea: 
+            - can only have a cycle when adding an edge where the two nodes already existed
+                - if the graph was already connected, 
+                - if was disconnected, then adding an edge can connect 
+        """
+        
+        graph = defaultdict(list)
+        def has_cycle(curr, parent):
+            if curr in visited:
+                return True
+            visited.add(curr)
+            for neighbor in graph[curr]:
+                if neighbor != parent:
+                    if has_cycle(neighbor, curr):
+                        return True
+            return False
+
+
+        for idx, edge in enumerate(edges):
+            x,y = edge
+            graph[x].append(y)
+            graph[y].append(x)
+
+            if x in graph and y in graph:
+                visited = set()
+                if has_cycle(x, -1):
+                    return edge
+                
+
+
+
+

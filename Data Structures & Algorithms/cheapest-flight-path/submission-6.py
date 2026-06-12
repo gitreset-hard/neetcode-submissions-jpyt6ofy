816ebfde7a8982a1ffec8bd080d30d64442e6481
letsgetit <71613715+gitreset-hard@users.noreply.github.com>
2026-06-12 12:04:49 -0600

    Add: cheapest-flight-path - submission-6

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-6.py
@@ -0,0 +1,33 @@
+from collections import defaultdict, deque
+import heapq
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        
+        graph = defaultdict(list)
+        for start, end, cost in flights:
+            graph[start].append((end, cost))
+
+        # need to track best price at each stop. the heap only tracks the current path it's on
+        prices = {stop:float('inf') for stop in range(n)}
+        prices[src] = 0
+        # a -> b
+        # a -> c -> a -> b
+        q = deque([(0, 0, src)])
+        while q:
+            cost, stops, city = q.popleft()
+            
+            # can't stop here. if the  k + 1 stop is the dst, it's fine
+            if stops > k:
+                continue
+            
+            for neighbor, next_cost in graph[city]:
+                cost_to_fly = cost + next_cost
+
+                if cost_to_fly <= prices[neighbor]:
+                    prices[neighbor] = cost_to_fly
+                    q.append((cost_to_fly, stops + 1, neighbor))
+    
+        return prices[dst] if prices[dst] != float('inf') else -1
+                
+
+    

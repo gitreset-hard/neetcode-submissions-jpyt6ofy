72f0026e6556e392d532a435176e1da7790fc784
letsgetit <71613715+gitreset-hard@users.noreply.github.com>
2026-07-06 16:32:34 -0600

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        graph = defaultdict(list)
+        for crs, pre in prerequisites:
+            graph[crs].append(pre)
+
+        visited = set() # cache
+        path = set() # to detect cycle
+        def hasCycle(crs):
+            # cycle found
+            if crs in path:
+                return True
+            if crs in visited:
+                return False
+            # track curr path
+            path.add(crs)
+
+            for pre in graph[crs]:
+                if hasCycle(pre):
+                    return True
+
+            visited.add(crs)
+            path.remove(crs)
+            return False
+
+
+        for idx in range(numCourses):
+            if idx not in visited:
+                if hasCycle(idx):
+                    return False
+        
+        return True
+
